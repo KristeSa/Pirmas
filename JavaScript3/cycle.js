ac10bab7fa964ff55f1725cf803cad2db4af8d28
KristeSa <kristina.saveiko@gmail.com>
2022-09-07 22:15:02 +0300

    nauji failai

---
+++ JavaScript3/cycle.js
@@ -0,0 +1,17 @@
+let i = 0;
+
+while (i < 4) {
+    console.log("Kristina")
+    i++
+}
+
+let combo = ""
+const firstName = "Kristina"
+let times = 5;
+
+do {
+    combo += firstName;
+    times--;
+} while (times > 0)
+console.log(combo);
+
