0903e9aa75703ec477db20a53cb5b3a6d832327e
KristeSa <110998803+KristeSa@users.noreply.github.com>
2022-10-04 11:09:46 +0300

    revision

--- JS/JS34/class.js
+++ JS/JS34/class.js
@@ -46,5 +46,20 @@ console.log(dublicateAllLetters("Kristina 678 Saveiko"));
 //Post code turi susidėti iš 5 skaičių, arba trijų skaičių ir dviejų raidžių. Jei yra tarpų - post code neteisingas.
 //Pvz: fn("abc123") => false; fn("12345") => true; fn("123ab") => true.
 
-function isPostCodeRight(x)
+const isValidPostcode = postCode => /^\d{5}|[0-9]{3}[A-Za-z]{2}$/.test(postCode);
 
+console.log(isValidPostcode('12584'));
+console.log(isValidPostcode('ght12'));
+console.log(isValidPostcode("125ka"));
+console.log(isValidPostcode('12sko'));
+
+
+//Sukurkite funkciją, kuri priims array ir prie kiekvieno array elemento pridės "7", nebent elementas baigsis "7".
+//pvz:
+// jazzify(["G", "F", "C"]) ➞ ["G7", "F7", "C7"]
+// jazzify(["Dm", "G", "E", "A"]) ➞ ["Dm7", "G7", "E7", "A7"]
+// jazzify(["F7", "E7", "A7", "Ab7", "Gm7", "C7"]) ➞ ["F7", "E7", "A7", "Ab7", "Gm7", "C7"]
+// jazzify([]) ➞ []
+
+const addNumberSeven = array => array.map(element => element.endsWith('7') ? element : element.concat('7'))
+console.log(addNumberSeven(["a1", "j5", "l7", "u8", "t9"]))
