140fa28b06566e245f5832cbc3fe71e14773eecf
KristeSa <110998803+KristeSa@users.noreply.github.com>
2022-10-24 16:56:53 +0300

    new

---
+++ JS/Fetch Delete/modules/main.js
@@ -0,0 +1,34 @@
+import { getProducts } from "./getProducts.js";
+
+const products = await getProducts();
+console.log(products);
+
+const renderProducts = () => {
+  const productCard = document.querySelector(".products-container");
+
+  products.forEach((element) => {
+    const img = document.createElement("img");
+    img.src = element.image;
+
+    const title = document.createElement("h4");
+    title.setAttribute("class", "product-title");
+    title.textContent = element.title;
+
+    const price = document.createElement("p");
+    price.setAttribute("class", "product-price");
+    price.textContent = `${element.price} EUR`;
+
+    const card = document.createElement("div");
+    card.setAttribute("class", "product-card");
+
+    const button = document.createElement("button");
+    button.setAttribute("class", "delete-button");
+    button.textContent = "Ištrinti";
+
+    card.append(img, title, price, button);
+
+    productCard.append(card);
+  });
+};
+
+renderProducts();
