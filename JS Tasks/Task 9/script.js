0160fa4f3ddc9dfa9559f58ad37182147f6220ee
KristeSa <110998803+KristeSa@users.noreply.github.com>
2022-10-27 16:54:07 +0300

    papildyta

--- JS Tasks/Task 9/script.js
+++ JS Tasks/Task 9/script.js
@@ -10,10 +10,15 @@ Metodas:
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
 
-class Movie {
-  constructor(title, director, budget) {
-    this.title = title;
-    this.direector = director;
-    this.budget = Number(budget).toFixed(0);
-  }
-}
+const movie = {
+  title,
+  director,
+  budget: function () {
+    const wasExpensive = this.budget > 100_000_000;
+    if (wasExpensive) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+};
